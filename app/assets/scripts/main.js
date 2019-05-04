96bd5d332c17eee9aea627d7a2d2187487274420
Nati <natan.kamusher@gmail.com>
2019-05-04 16:09:22 +0300

    Cleanup old files and completed the stats  section

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,3 +1,34 @@
 import '../styles/main.scss';
+import $ from 'jquery';
 import 'materialize-css';
 import './materializeSetup';
+
+// Counter
+
+$('.section').hide();
+
+setTimeout(function() {
+  $(document).ready(function() {
+    // Hide preloader
+    $('.loader').fadeOut();
+
+    // Show sections
+    $('.section').fadeIn();
+    $('.count').each(function() {
+      $(this)
+        .prop('Counter', 0)
+        .animate(
+          {
+            Counter: $(this).text()
+          },
+          {
+            duration: 1000,
+            easing: 'swing',
+            step: function(now) {
+              $(this).text(Math.ceil(now));
+            }
+          }
+        );
+    });
+  });
+}, 1000);
